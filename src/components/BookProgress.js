f53823b6c1cd3bbda0a732be12b236c58217c2b2
jamcry <jamcry@hotmail.com>
2019-07-27 11:55:57 +0300

    Add circular progress bar

--- src/components/BookProgress.js
+++ src/components/BookProgress.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function BookProgress(props) {
+  const percentage = ((props.currentPages / props.totalPages)*100).toFixed(1);
   return(
     <div className="book-progress">
-      <h1>{(props.currentPages / props.totalPages).toFixed(1)} %</h1>
+      <CircularProgressbar
+        value={percentage}
+        text={`${percentage}%`}
+        background
+        backgroundPadding={6}
+        styles={buildStyles({
+          backgroundColor: "#3e98c7",
+          textColor: "#fff",
+          pathColor: "#fff",
+          trailColor: "transparent"
+        })}
+      />
     </div>
   );
 }
