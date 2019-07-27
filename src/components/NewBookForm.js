926de77f346ae959334cb63e028d656a6c702097
jamcry <jamcry@hotmail.com>
2019-07-27 17:26:23 +0300

    Refactor new book form

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,48 +1,70 @@
-import React from 'react';
+import React from "react";
 import "./NewBookForm.css";
 
 export default class NewBookForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      title: '',
-      author: '',
+      title: "",
+      author: "",
       pages: 0,
-      coverUrl: '',
-      currentPages: 0,
-    }
+      coverUrl: "",
+      currentPages: 0
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
-    this.props.addNewBook({...this.state});
+    this.props.addNewBook({ ...this.state });
+    e.target.reset();
     e.preventDefault();
   }
 
   render() {
-    return(
+    return (
       <form className="form" onSubmit={this.handleSubmit}>
-        New Book
+        <h1>New Book</h1>
         <label>
           Title
-          <input type="text" name="title" onChange={this.handleChange} />
+          <input 
+            type="text"
+            name="title"
+            onChange={this.handleChange}
+            value={this.state.title}
+            required
+          />
         </label>
         <label>
           Author
-          <input type="text" name="author" onChange={this.handleChange} />
+          <input 
+            type="text"
+            name="author"
+            onChange={this.handleChange}
+            value={this.state.author}
+          />
         </label>
         <label>
           Pages
-          <input type="number" name="pages" onChange={this.handleChange} />
+          <input 
+            type="number"
+            name="pages"
+            onChange={this.handleChange}
+            value={this.state.pages}
+          />
         </label>
         <label>
           Current Pages
-          <input type="number" name="currentPages" onChange={this.handleChange} />
+          <input
+            type="number"
+            name="currentPages"
+            onChange={this.handleChange}
+            value={this.state.currentPages}
+          />
         </label>
         <button className="btn">Add</button>
       </form>
