8d48b9e01f6116312e5a70dd62b289000fbb4a30
jamcry <jamcry@hotmail.com>
2019-07-27 17:21:47 +0300

    Add placeholder book cover photo

--- src/components/BookCover.js
+++ src/components/BookCover.js
@@ -1,11 +1,12 @@
 import React from "react";
+import default_cover from "../assets/img/default_cover.png"
 
 function BookCover(props) {
   return (
     <div className="book-cover">
       <img
         className="book-cover-image"
-        src={props.url}
+        src={props.url || default_cover}
         alt={`${props.title}'s book cover`}
         width="75"
         height="111"
