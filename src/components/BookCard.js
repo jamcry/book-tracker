afbc41e8cd729b48c628fbddff16efc03c026dfb
jamcry <jamcry@hotmail.com>
2019-07-28 16:38:08 +0300

    Getting ready for deployment to gh-pages

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -14,8 +14,14 @@ class BookCard extends React.Component {
   }
 
   handleDelete() {
-    if (window.confirm(`You are deleting the book "${this.props.data.title}" from your library.`))
-      this.props.actions.removeBook(this.props.data)
+    if (
+      window.confirm(
+        `You are deleting the book "${
+          this.props.data.title
+        }" from your library.`
+      )
+    )
+      this.props.actions.removeBook(this.props.data);
   }
 
   render() {
