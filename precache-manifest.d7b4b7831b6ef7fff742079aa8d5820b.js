a8ac43b385a8b3c5568d9dd8017585e8813f9c31
jamcry <jamcry@hotmail.com>
2019-07-28 16:39:11 +0300

    Updates

---
+++ precache-manifest.d7b4b7831b6ef7fff742079aa8d5820b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d33ecc51074a5341e80e89fac3c077",
+    "url": "/book-tracker/index.html"
+  },
+  {
+    "revision": "9840c809a971c1c1406c",
+    "url": "/book-tracker/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "88431f430f2357c4cee9",
+    "url": "/book-tracker/static/css/main.cc8d7eae.chunk.css"
+  },
+  {
+    "revision": "9840c809a971c1c1406c",
+    "url": "/book-tracker/static/js/2.0bbbe0e1.chunk.js"
+  },
+  {
+    "revision": "88431f430f2357c4cee9",
+    "url": "/book-tracker/static/js/main.dc83d77e.chunk.js"
+  },
+  {
+    "revision": "5a1bbcc1e5d6dd9f2136",
+    "url": "/book-tracker/static/js/runtime~main.097e1299.js"
+  }
+]);
