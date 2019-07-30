9f4d09d8cc4f7dbc5f7986dac4d414b5232e058b
jamcry <jamcry@hotmail.com>
2019-07-30 19:11:41 +0300

    Improve BookActions layout

--- src/components/BookActions.js
+++ src/components/BookActions.js
@@ -4,29 +4,35 @@ import Popup from "reactjs-popup";
 export default function BookActions(props) {
   return (
     <div className="book-actions row">
-      <Popup
-        trigger={<button className="btn-inline btn-edit">Edit</button>}
-        modal
-      >
-        <div className="edit-popup-content">
-          <h1>Edit Details</h1>
-          <p>This feature is coming soon.</p>
-        </div>
-      </Popup>
+      <div style={{ flex: 1 }} />
+      
+      <div style={{ display: "flex", flex: 3, padding: 10 }}>
+        <Popup
+          trigger={<button className="btn-inline btn-edit">Edit</button>}
+          modal
+        >
+          <div className="edit-popup-content">
+            <h1>Edit Details</h1>
+            <p>This feature is coming soon.</p>
+          </div>
+        </Popup>
 
-      <button className="btn-inline btn-delete" onClick={props.handleDelete}>
-        Delete
-      </button>
+        <button className="btn-inline btn-delete" onClick={props.handleDelete}>
+          Delete
+        </button>
+      </div>
 
-      <Popup
-        trigger={<button className="btn btn-update">Update</button>}
-        modal
-      >
-        <div className="update-popup-content">
-          <h1>Update Progress</h1>
-          <p>This feature is coming soon.</p>
-        </div>
-      </Popup>
+      <div style={{ flex: 1, textAlign: "center", padding: 10 }}>
+        <Popup
+          trigger={<button className="btn btn-update">Update</button>}
+          modal
+        >
+          <div className="update-popup-content">
+            <h1>Update Progress</h1>
+            <p>This feature is coming soon.</p>
+          </div>
+        </Popup>
+      </div>
     </div>
   );
 }
