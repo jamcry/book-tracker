d88158c069eda57e7eb952fc6959c964854247f9
jamcry <jamcry@hotmail.com>
2019-07-30 19:20:59 +0300

    Improve main layout and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Book Tracker</h1>
-      <h3>Library and book management app</h3>
-      <p>Create your online library and track your progress.</p>
+      <Header />
       <BookList />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return(
+    <div className='Header'>
+      <h1>Book Tracker</h1>
+      <h3>Library and book management app</h3>
+      <p>Create your online library and track your progress.</p>
+    </div>
+  );
+}
