30bb26102147a7c772df8c58d55b62c481ed589e
jamcry <jamcry@hotmail.com>
2019-07-30 18:55:13 +0300

    Fix 'Failed propType' error in BookActions

--- src/components/BookActions.js
+++ src/components/BookActions.js
@@ -8,17 +8,24 @@ export default function BookActions(props) {
         trigger={<button className="btn-inline btn-edit">Edit</button>}
         modal
       >
-        <h1>Edit Details</h1>
-        <p>This feature is coming soon.</p>
+        <div className="edit-popup-content">
+          <h1>Edit Details</h1>
+          <p>This feature is coming soon.</p>
+        </div>
       </Popup>
 
       <button className="btn-inline btn-delete" onClick={props.handleDelete}>
         Delete
       </button>
 
-      <Popup trigger={<button className="btn btn-update">Update</button>} modal>
-        <h1>Update Progress</h1>
-        <p>This feature is coming soon.</p>
+      <Popup
+        trigger={<button className="btn btn-update">Update</button>}
+        modal
+      >
+        <div className="update-popup-content">
+          <h1>Update Progress</h1>
+          <p>This feature is coming soon.</p>
+        </div>
       </Popup>
     </div>
   );
